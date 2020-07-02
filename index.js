79c5d813e5043d121757749c03096b52e74b3787
Hussein Talal <hossientalal2@gmail.com>
2020-07-02 21:15:57 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let result = "undefined"
+
+   result = arr.find(object => object.result === "W")
+
+  return !!result ? result.year : result;
+
+}
